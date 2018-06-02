35fb184f41b22f5849788e967370db100b1e6e99
nmorozov <nikmorze@gmail.com>
2018-06-02 21:47:08 +0400

    New routes were added. Code style reformat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { ServicesComponent }    from './services/services.component';
+import { ServicesComponent } from './services/services.component';
 import { ServiceDetailComponent } from './service-detail/service-detail.component';
+import { SpecialistsComponent } from './specialists/specialists.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/services', pathMatch: 'full' },
-  { path: 'services', component: ServicesComponent },
-  { path: 'services/:id', component: ServiceDetailComponent },
+	{ path: '', redirectTo: '/services', pathMatch: 'full' },
+	{ path: 'services', component: ServicesComponent },
+	{ path: 'notfound', component: NotfoundComponent },
+	{ path: 'services/:id', component: ServiceDetailComponent },
+	{ path: 'specialists', component: SpecialistsComponent },
+	{ path: 'checkout', component: CheckoutComponent },
+	{ path: '**', redirectTo: '/notfound', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-
-
 export class AppRoutingModule {}
