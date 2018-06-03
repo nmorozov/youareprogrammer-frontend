ffa3b3a91861ff67d3db7e5a38f29071ba21c8d4
nmorozov <nikmorze@gmail.com>
2018-06-03 20:25:50 +0400

    Code restyle. Detailt service route was removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ServicesComponent } from './services/services.component';
-import { ServiceDetailComponent } from './service-detail/service-detail.component';
-import { SpecialistsComponent } from './specialists/specialists.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+// Components
+import { ServicesComponent } from './components/services/services.component';
+import { SpecialistsComponent } from './components/specialists/specialists.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/services', pathMatch: 'full' },
 	{ path: 'services', component: ServicesComponent },
 	{ path: 'notfound', component: NotfoundComponent },
-	{ path: 'services/:id', component: ServiceDetailComponent },
 	{ path: 'specialists', component: SpecialistsComponent },
 	{ path: 'checkout', component: CheckoutComponent },
 	{ path: '**', redirectTo: '/notfound', pathMatch: 'full' }
