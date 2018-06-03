a10ce87566c17fb3734cfc93724dff1c183117df
nmorozov <nikmorze@gmail.com>
2018-06-03 20:30:33 +0400

    app code style reformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HttpClientModule } from '@angular/common/http';
-import { ArrayHelper } from '../utils/ArrayHelper';
+import { ArrayHelper } from './utils/ArrayHelper';
+import { ServiceStatistic } from './components/services/services-statistic';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { ServicesComponent } from './services/services.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { ServiceDetailComponent } from './service-detail/service-detail.component';
-import { SpecialistsComponent } from './specialists/specialists.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+
+// Components
+import { AppComponent } from './app.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SpecialistsComponent } from './components/specialists/specialists.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		ServicesComponent,
-		ServiceDetailComponent,
-		SpecialistsComponent,
-		NotfoundComponent,
-		CheckoutComponent
+	declarations: [ AppComponent, ServicesComponent, SpecialistsComponent, NotfoundComponent, CheckoutComponent ],
+	imports: [
+		MDBBootstrapModule.forRoot(),
+		BrowserModule,
+		HttpClientModule,
+		AppRoutingModule,
+		FormsModule,
+		ArrayHelper,
+		ServiceStatistic
 	],
-	imports: [ BrowserModule, MDBBootstrapModule.forRoot(), HttpClientModule, AppRoutingModule, ArrayHelper, FormsModule ],
 	schemas: [ NO_ERRORS_SCHEMA ],
 	providers: [],
 	bootstrap: [ AppComponent ]
